599df4efe6c0b626be037795e4106952fb0f7a1e
Swayam <swayamgagneja12345@gmail.com>
2026-04-02 01:51:23 +0530

    Currency Converter Basic Working Done

---
+++ app/src/main/java/com/example/currencyconverter/MainActivity.java
@@ -0,0 +1,77 @@
+package com.example.currencyconverter;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText amount;
+    Spinner currencyFrom;
+    Spinner currencyTo;
+    Button submitBtn;
+    TextView answerTV;
+
+
+    public double convert(double amt, String from, String to){
+        if(from.equals(to))return amt;
+        double toINR = 0;
+        if(from.equals("USD")){
+            toINR = amt*93;
+        }
+        if(from.equals("EUR")){
+            toINR = amt*109;
+        }
+        if(from.equals("JPY")){
+            toINR = amt*0.6;
+        }
+        double finalAmt=0;
+
+        if(to.equals("USD")){
+            finalAmt = toINR/93;
+        }
+        if(to.equals("JPY")){
+            finalAmt = toINR/0.6;
+        }
+        if(to.equals("EUR")){
+            finalAmt = toINR/109;
+        }
+        if(to.equals("INR")){
+            finalAmt = toINR;
+        }
+
+        return finalAmt;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        answerTV = findViewById(R.id.answerTV);
+        amount = findViewById(R.id.amount);
+        currencyFrom = findViewById(R.id.currencyFrom);
+        currencyTo = findViewById(R.id.currencyTo);
+        submitBtn = findViewById(R.id.submitBtn);
+        submitBtn.setOnClickListener(v -> {
+            double amt = Double.parseDouble(amount.getText().toString());
+            String from = currencyFrom.getSelectedItem().toString();
+            String to = currencyTo.getSelectedItem().toString();
+            double ans = convert(amt,from,to);
+            answerTV.setText(String.valueOf(ans));
+        });
+    }
+}
